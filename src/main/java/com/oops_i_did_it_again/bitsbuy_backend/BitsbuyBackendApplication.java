65223b3616d271afd1011616f10175721848ab2f
Manish Vasireddy <128839718+akamikado@users.noreply.github.com>
2023-09-28 00:42:42 +0530

    Add files via upload

---
+++ src/main/java/com/oops_i_did_it_again/bitsbuy_backend/BitsbuyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.oops_i_did_it_again.bitsbuy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitsbuyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitsbuyBackendApplication.class, args);
+	}
+
+}
